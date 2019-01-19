fd10c417335ed3fe8c341738252e929b28ad79b9
nelsondude <alexn1336@gmail.com>
2019-01-19 15:51:27 -0500

    Working tables and contact section

--- home/models.py
+++ home/models.py
@@ -1,24 +1,42 @@
 from django.db import models
+from django.utils import timezone
 from modelcluster.fields import ParentalKey
+from wagtail.contrib.table_block.blocks import TableBlock
+from wagtail.core.blocks import CharBlock
 
 from wagtail.core.models import Page, Orderable
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, StreamFieldPanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
-
-import datetime
 
 
 class HomePage(Page):
     about_content = RichTextField(blank=True)
     layout_lab_link = models.URLField()
 
+    # Contact Info
+    address = models.CharField(max_length=200, null=True)
+    email = models.EmailField(null=True)
+    phone = models.CharField(max_length=100, null=True)
+
+    hours = StreamField([
+        ('hour_info', CharBlock())
+    ], null=True)
+
     content_panels = Page.content_panels + [
         InlinePanel('announcements', label='Announcements'),
         FieldPanel('about_content', classname="full"),
         InlinePanel('gallery_images', label="Gallery images"),
-        FieldPanel('layout_lab_link')
+        FieldPanel('layout_lab_link'),
+        InlinePanel('pricing_sections', label='Pricing Sections'),
+        MultiFieldPanel([
+            FieldPanel('address'),
+            FieldPanel('email'),
+            FieldPanel('phone'),
+            StreamFieldPanel('hours')
+        ], heading='Contact Information')
     ]
+
 
 class GalleryImage(Orderable):
     page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='gallery_images')
@@ -32,14 +50,30 @@ class GalleryImage(Orderable):
         FieldPanel('caption'),
     ]
 
+
 class Announcement(Orderable):
     page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='announcements')
     announcement = models.CharField(blank=True, max_length=200)
-    start_date = models.DateField(default=datetime.date.today(), null=False)
+    start_date = models.DateField(default=timezone.now(), null=False)
     end_date = models.DateField(null=True)
 
     panels = [
         FieldPanel('announcement'),
         FieldPanel('start_date'),
         FieldPanel('end_date')
+    ]
+
+
+class PricingSection(Orderable):
+    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='pricing_sections')
+    pricing_title = models.CharField(max_length=200)
+    caption = models.CharField(max_length=200)
+    body = StreamField([
+        ('table', TableBlock(), ),
+    ], blank=True)
+
+    panels = [
+        FieldPanel('pricing_title'),
+        FieldPanel('caption'),
+        StreamFieldPanel('body'),
     ]
--- lbg_site/settings/base.py
+++ lbg_site/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'wagtail.search',
     'wagtail.admin',
     'wagtail.core',
+    "wagtail.contrib.table_block",
 
     'modelcluster',
     'taggit',
