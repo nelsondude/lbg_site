5f04c125b6793f8a1c5902c8599ddb2f19fa749e
nelsondude <alexn1336@gmail.com>
2019-01-31 19:15:47 -0500

    Include autoprefixer to support more browser types in css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 const gulp = require('gulp');
 const livereload = require('gulp-livereload');
 const sass = require('gulp-sass');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
 
 sass.compiler = require('node-sass');
 
 const cssMatch = ['**/*.scss', '!node_modules/**/*.scss'];
 
 gulp.task('scss', function () {
+  var processors = [
+        autoprefixer
+    ];
   return gulp.src(cssMatch, {base: './'})
     .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(processors))
     .pipe(gulp.dest(''))
     .pipe(livereload());
 });
