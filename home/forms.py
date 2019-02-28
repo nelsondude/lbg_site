39af444c3218c71938aa18360b82602edb13c61d
nelsondude <alexn1336@gmail.com>
2019-02-28 12:42:29 -0500

    Set rows for contact form text area
    Make smaller so that the whole form fits on the screen

--- home/forms.py
+++ home/forms.py
@@ -5,7 +5,7 @@ from captcha.fields import CaptchaField
 class ContactForm(forms.Form):
     from_email = forms.EmailField(required=True, label='Your Email')
     subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    message = forms.CharField(widget=forms.Textarea(attrs={'rows': 4}), required=True)
     phone = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel'}), required=False, label='Phone Number')
     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), required=False,
                                  label='Attachments (multiple is allowed)')
